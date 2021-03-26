9c6bb3b32891dec8dd7400c71969b7de9a296206
EC2 Default User <ec2-user@ip-172-31-3-104.ap-northeast-1.compute.internal>
2021-03-26 15:44:28 +0000

    first commit

--- js/jquery.js
+++ js/jquery.js
@@ -56,7 +56,7 @@ $(function(){
         var elemPos = $(this).offset().top,
             scroll = $(window).scrollTop(),
             windowHeight = $(window).height();
-          if (scroll > elemPos - windowHeight + 400){
+          if (scroll > elemPos - windowHeight + 200){
               $(this).addClass('scrollin');
             }
         });
